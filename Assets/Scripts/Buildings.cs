11e1900afb64a057e7ae4a4baf44790808a89eb2
Connor-Hughes <hughesconnor6r@gmail.com>
2019-10-18 17:41:23 +0200

    Basic set up of RTS Game

---
+++ Assets/Scripts/Buildings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    abstract class Building // all properties for the buildings
+    {
+        protected int posX;
+
+        protected int posY;
+
+        protected int health;
+
+        public Faction faction;
+
+        protected string symbol;
+
+
+        public abstract bool Destruction();
+
+        public abstract string ToString();
+
+        public Building(int x, int y, int hp, Faction fac, string sym) //constructor for the building class
+        {
+            posX = x;
+            posY = y;
+            health = hp;
+            faction = fac;
+            symbol = sym;
+
+        }
+
+    }
+}
